8cf95acf8e063d4cc8269cf3ff9a0e993b31b225
DanArmstrong124 <danielarmstrong124@gmail.com>
2022-09-12 23:46:54 +0000

    Test

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
-var request = new XMLHttpRequest()
+var request = new XMLHttpRequest("Ocp-Apim-Subscription-Key: a69d02c0ac63490988ee6415afef4318")
 
 // Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://api.sportsdata.io/v3/mlb/scores/json/AreAnyGamesInProgress', "Ocp-Apim-Subscription-Key: a69d02c0ac63490988ee6415afef4318")
+request.open('GET', 'https://api.sportsdata.io/v3/mlb/scores/json/AreAnyGamesInProgress')
 
 request.onload = function () {
   // Begin accessing JSON data here
