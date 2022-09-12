0e661c0c8fd721c94ea263503f809cd3c063a95d
DanArmstrong124 <danielarmstrong124@gmail.com>
2022-09-12 23:39:38 +0000

    hi

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 var request = new XMLHttpRequest()
 
 // Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://api.sportsdata.io/v3/mlb/odds/json/BettingPlayerPropsByGameID/{gameId}', Ocp-Apim-Subscription-Key: {key})
+request.open('GET', 'https://api.sportsdata.io/v3/mlb/scores/json/AreAnyGamesInProgress', "a69d02c0ac63490988ee6415afef4318")
 
 request.onload = function () {
   // Begin accessing JSON data here
